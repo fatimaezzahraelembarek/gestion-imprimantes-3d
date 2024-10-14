2989f97afe19d49f20c79010ffa62641e4005fd1
lembafat <lembafat@sct-22-3044l.ur.univ-rouen.fr>
2024-10-14 09:18:37 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Gestion-d’imprimantes-3D',
+  webDir: 'www'
+};
+
+export default config;
